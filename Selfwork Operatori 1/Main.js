b50495dbe6017159307daf20f12690a1ec5515e3
FaustNFurious <lorelego2003@gmail.com>
2024-11-09 21:47:20 +0100

    Add files via upload
    svolto esercizio operatori 1

---
+++ Selfwork Operatori 1/Main.js
@@ -0,0 +1,14 @@
+// Dichiarazione Variabili
+let num1 = 4;
+let num2 = 40;
+let num3 = 2;
+let num4 = 80;
+let num5 = 5;
+
+// Somma e Media delle variabili
+let somma = num1 + num2 + num3 + num4 + num5;
+let media = (num1 + num2 + num3 + num4 + num5) / 5;
+
+// Stampa in console i valori
+console.log('La somma tra i numeri equivale a: ' + somma);
+console.log('La media equivale a: ' + media.toFixed(2));
